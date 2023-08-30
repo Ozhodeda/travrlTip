849e50bb4f0f2323ce1acaeaa5dd812212069906
itamarsa <itamarr87@gmail.com>
2023-08-30 16:05:46 +0300

    declare render

--- js/app.controller.js
+++ js/app.controller.js
@@ -17,6 +17,7 @@ function onInit() {
 
         })
         .catch(() => console.log('Error: cannot init map'))
+        renderLocation()
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
